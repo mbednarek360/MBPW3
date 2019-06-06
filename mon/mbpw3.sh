af9e7efd52c4cae627419372389395a10cb0182b
mbednarek360 <mbednarek360@protonmail.com>
2019-06-05 23:26:52 -0400

    Initial Commit

---
+++ mon/mbpw3.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+host="mbpw3.us.to"
+ports=(80 443 9090)
+sshs=(22 20021 20022)
+timeout=1
+
+# make sure proper ports are open
+function portCheck {
+    for port in "${ports[@]}"
+    do
+        res=$(nc -zv -w"$timeout" "$host" "$port" 2>&1)
+        if [[ "$res" != *"refused"* ]]
+        then
+            echo "Port Open: $(echo $res | awk -F"[()]" '{print $2}')"
+        else
+            echo "Port Closed: $port"
+        fi
+    done    
+}
+
+# verify ssh connections
+function sshCheck {
+    for port in "${sshs[@]}"
+    do
+        name=$(ssh -o ConnectTimeout="$timeout" root@"$host" -p "$port" "hostname" 2>/dev/null) 
+        if [ "$?" = "0" ]
+        then
+          echo "SSH Successful: $name"
+        else
+          echo "SSH Failed: $port"
+        fi
+    done
+}
+
+# resolve hostname
+function domainCheck {
+    ip=$(getent hosts "$host" | awk '{ print $1 }')
+    if [ -n "$ip" ]
+    then
+        echo "Domain Resolved: $ip"
+    else
+        echo "Resolve Failed"
+    fi
+}
+
+# check temperature
+function tempCheck {
+    for port in "${sshs[@]}"
+    do
+        tempCmd="hostname | tr -d '\n' && echo ': ' | tr -d '\n' && cat /sys/class/thermal/thermal_zone*/temp | tail -1"
+        ssh -o ConnectTimeout="$timeout" root@"$host" -p "$port" "$tempCmd" 2>/dev/null
+    done 
+}
+
+# display starting info
+clear
+echo "Host: ${host[@]}"
+echo "Ports: ${ports[@]}"
+echo "SSH: ${sshs[@]}"
+echo
+
+# handle flags
+case "$1" in 
+    -p)
+        portCheck
+        ;;
+    
+    -s)
+        sshCheck
+        ;;
+    -d)
+        domainCheck
+        ;;
+    -t)
+        tempCheck
+        ;;
+    *)
+        domainCheck
+        sshCheck  
+        portCheck
+        ;;
+esac
